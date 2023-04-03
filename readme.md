08b9385798312eb12b38d32cf0500f1d03eae7d4
yokubjanovich <yokubjanovich@gmail.com>
2023-04-03 11:28:15 +0500

    time bn bulgan muomo yuqaldi

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
+## math quiz app v1
 -- sonlarni va ammallarni random qilish👍🏿
 -- to'gri jovobni berish👍🏿
 -- to'gri javobga yaqin javoblarni hosil qilish👍🏿
@@ -8,16 +9,22 @@
 -- 3 qolgan buttonlarni qiymati resultdagi qiymat bilan bir hil bulmasligi kerak👍🏿
 -- 4 buttonlarni qolgan buttonlarga qiymat berayotganda javob kiritilgan button inobotga olmasligi kerak => boshqacha yul qildim yani turtla qiymatni ham oldib urnini render qilib quyib yubordim👍🏿
 
--- math-quiz-app version 2
+## math-quiz-app v1
 
 -- 1 uyinga kirganda start degan button bulishi kerak ushani bosgandan kiyin uyin boshlanishi kerak👍🏿
--- 2 har bir rauntga 10 soniya vaqt berish kerak
--- 3 agar vaqtida topsa qolgan vaqtning kiyingi uyinga qushib berishi kerak
--- 4 gar to'g'ri javob bera olmasa kiyingi rauntning 2 sekundini olib quyishi kerak
--- 5 agar uyin boshlangandan kiyin 5 marta ketma-ket xato qilsa game over bulishi kerak
--- 6 uyinning 15 ta raunti bulishi kerak
--- 7 uyinning pastida bitta bulim bulishi kerak va unda 15 dumoloq bulim bulishi kerak
--- 8 agar to'g'ri javob berilsa rangi ko'k xato javo berilsa qizil vaqti uzib ketsa qora rang buliwi kk
--- 9 ohirida qizillarni qoralarni qushib umumiysini qaytarish kerak ko'klarnig ham umumiysini qaytarish kerak.
+-- 2 har bir rauntga 10 soniya vaqt berish kerak👍🏿
+-- 3 agar vaqtida topsa qolgan vaqtning kiyingi uyinga qushib berishi kerak👍🏿
+-- 4 gar to'g'ri javob bera olmasa kiyingi rauntning 2 sekundini olib quyishi kerak yani 10 - 2👍🏿
+-- 5 agar uyin boshlangandan kiyin 5 marta ketma-ket xato qilsa game over bulishi kerak ❎
+-- 6 uyinning 10 ta raunti bulishi kerak👍🏿
+-- 7 uyinning pastida bitta bulim bulishi kerak va unda 10 dumoloq bulim bulishi kerak👍🏿
+-- 8 agar to'g'ri javob berilsa rangi ko'k xato javo berilsa qizil vaqti uzib ketsa qora rang buliwi kk 👍🏿
+-- 9 ohirida qizillarni qoralarni qushib umumiysini qaytarish kerak ko'klarnig ham umumiysini qaytarish kerak.👇🏼
+
+## update v3
+- uyin tugagandan kiyin buttonlar deseable bulib qolishi kerak;
+- vaqt bilan muoni to'g'irlash kerak
+- raund 10 bulganda umumiy natijani qaytarish kerak;
+- kodlarni clean qilish kerak;
 
 
--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 // variables
-let result, interval, chooseElm, value1, value2, correctIndex;
+let result, interval, chooseElm, value1, value2, correctIndex, btnInterval;
 let num1 = document.querySelector(".num-1");
 let num2 = document.querySelector(".num-2");
-let gameScore = 1;
+let gameScore = 1,
+  levelTime,
+  oldTime = 0;
 
 // const variables
 const mathElm = document.querySelector(".math-elm");
@@ -13,14 +15,15 @@ const btnStart = document.querySelector(".btnStart");
 const firstSection = document.querySelector(".start");
 const gameTime = document.querySelector(".game-time");
 const gameLevel = document.querySelector(".game-level");
-
+const scoreChild = document.querySelectorAll(".scoreChild");
 const mathElm2 = ["+", "-", "*"];
-mainFunc();
+
+// mainFunc();
+
 function mainFunc() {
-  // random functions
+  // random element
   function randomNum() {
-    // min and max included
-    return Math.floor(Math.random() * (20 - 0 + 1) + 0);
+    return Math.floor(Math.random() * (20 + 1));
   }
 
   //choose math operator
@@ -43,7 +46,7 @@ function mainFunc() {
   // answer[btnForResult].textContent = result;
 
   let mainAnwes = [result, result + 2, result - 2, result + 1];
-  mainAnwes = mainAnwes.sort((a, b) => Math.random() - 0.5);
+  mainAnwes = mainAnwes.sort(() => Math.random() - 0.5);
   // find the correct index
   mainAnwes.filter((item, idx) => {
     if (item === result) {
@@ -51,9 +54,16 @@ function mainFunc() {
     }
   });
 
-  // check correct btn
+  // qiymatlarni buttonlarga berish
   answer.forEach((item, idx) => {
     item.textContent = mainAnwes[idx];
+  });
+  findCorrectAnswer();
+}
+
+// check correct btn
+function findCorrectAnswer() {
+  answer.forEach((item) => {
     item.addEventListener("click", () => {
       if (item.textContent == result) {
         item.classList.add("correct");
@@ -64,54 +74,94 @@ function mainFunc() {
       answer.forEach((item) => {
         item.style.pointerEvents = "none";
       });
-      setTimeout(() => {
-        restoreFunc();
+      clearInterval(btnInterval);
+      btnInterval = setTimeout(() => {
+        if (gameScore <= 10) {
+          restoreFunc();
+          mainFunc();
+        }
+        console.log("hello");
       }, 1000);
     });
   });
 }
 // function restore game
 function restoreFunc() {
-  mainFunc();
   answer.forEach((item) => {
-    item.style.pointerEvents = "all";
+    if (gameScore <= 10) {
+      item.style.pointerEvents = "all";
+    } else {
+      item.style.pointerEvents = "none";
+    }
     item.classList.remove("correct");
     item.classList.remove("wrong");
   });
 }
-
 // game start sevtion
 
-// btnStart.addEventListener("click", () => {
-//   firstSection.style.display = "none";
-//   mainFunc();
-// });
+btnStart.addEventListener("click", () => {
+  roundTime();
+  firstSection.style.display = "none";
+  mainFunc();
+});
 
 // round time
 function roundTime() {
-  let i = 10;
+  levelTime = 10;
+  levelTime += oldTime;
   interval = setInterval(() => {
-    if (i <= 0) {
-      clearInterval(interval);
-      mainFunc();
-      roundTime();
-      gamelevel();
+    let minutes = "00",
+      seconds;
+    if (levelTime >= 60) {
+      minutes = Math.floor(levelTime / 60, 10);
+      minutes = minutes < 10 ? "0" + minutes : minutes;
     }
-    if (i < 10) {
-      gameTime.textContent = `time: 00:0${i--}`;
-    } else {
-      gameTime.textContent = `time: 00:${i--}`;
+    levelTime--;
+    seconds = Math.floor(levelTime % 60, 10);
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    gameTime.textContent = `time: ${minutes}:${seconds}`;
+    if (levelTime <= 0) {
+      oldTime = -2;
+      scoreChild[gameScore - 1].classList.add("timeOut");
+      clearInterval(interval);
+      roundTime();
+      mainFunc();
+      levelFunc();
     }
   }, 1000);
 }
-roundTime();
 
 // round level
-function gamelevel() {
+function levelFunc() {
   ++gameScore;
-  if (gameScore <= 2) {
+  if (gameScore <= 10) {
     gameLevel.textContent = `level: ${gameScore}/10`;
   } else {
     clearInterval(interval);
+    levelTime = 10;
+    gameTime.textContent = `time: 00:${levelTime}`;
   }
 }
+
+// anwer larni bosganda level oshishi va vaqt yangilanishi uchun
+answer.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.textContent == result) {
+      oldTime = levelTime;
+      clearInterval(interval);
+      scoreChild[gameScore - 1].classList.add("win");
+      roundTime();
+      setTimeout(() => {
+        levelFunc();
+      }, 1000);
+    } else {
+      oldTime = -2;
+      clearInterval(interval);
+      scoreChild[gameScore - 1].classList.add("lose");
+      roundTime();
+      setTimeout(() => {
+        levelFunc();
+      }, 1000);
+    }
+  });
+});
